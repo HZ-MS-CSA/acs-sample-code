7937aab0589950e40864a1092b06a945af0a6c36
Han Zhang <zhha@microsoft.com>
2021-01-29 00:30:14 +0000

    add instructions

---
+++ create_folders.py.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[3]:
+
+
+target_phone_numbers_dir = './target_phone_numbers/'
+os.makedirs(target_phone_numbers_dir, exist_ok=True)
+required_info_dir = './required_info/'
+os.makedirs(required_info_dir, exist_ok=True)
+output_dir = './output/'
+os.makedirs(output_dir, exist_ok=True)
+archive_dir = './archive/'
+os.makedirs(archive_dir, exist_ok=True)
+print(f"{target_phone_numbers_dir}, {required_info_dir}, {output_dir}, {archive_dir} are created")
+
+
+# In[ ]:
+
+
+
+
